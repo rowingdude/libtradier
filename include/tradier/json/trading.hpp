7a57e0c6d424af88db08ef1faf4f034c8676a3f9
Benjamin Cance <canceb@gmail.com>
2025-05-22 16:59:31 -0400

    Updates to header

--- include/tradier/json/trading.hpp
+++ include/tradier/json/trading.hpp
@@ -1,3 +1,15 @@
+/*
+ * libtradier - Tradier API C++ Library v0.1.0
+ *
+ * Author: Benjamin Cance (kc8bws@kc8bws.com)
+ * Date: 2025-05-22
+ *
+ * This software is provided free of charge under the MIT License.
+ * By using it, you agree to absolve the author of all liability.
+ * See LICENSE file for full terms and conditions.
+ */
+
+
 #pragma once
 
 #include <nlohmann/json.hpp>
